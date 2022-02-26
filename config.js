508ee3f58241eadb5d8edd1d91d5c4fa5d2efe0a
NekoKyun <91820453+Yuri-Neko@users.noreply.github.com>
2022-02-26 13:19:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 let fs = require('fs')
 
-global.owner = ['6288279268363'] // Put your number here
+global.owner = ['6283825121214','6282112080081'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json'))
 global.APIs = { // API Prefix
